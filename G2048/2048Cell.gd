2453a999a679cd717c27b66ee89038c781c7c075
xiaoxiaole <sqrtthreexiao@gmail.com>
2024-09-26 21:56:51 +0800

    完善2048

--- G2048/2048Cell.gd
+++ G2048/2048Cell.gd
@@ -1,16 +1,57 @@
-extends Node
+extends Control
 
-@export var grid_pos : Vector2i = Vector2i.ZERO
-@export var number : int = 2
+@export var grid_pos: Vector2i = Vector2i.ZERO
+@export var number: int = 2
 @onready var label: Label = $Label
+@onready var nine_patch_rect: NinePatchRect = $NinePatchRect
+
+var tween
+var color_dict = {
+	2: Color.WHITE,
+	2 << 1: Color("90646c"),
+	2 << 2: Color.AQUA,
+	2 << 3: Color.AQUAMARINE,
+	2 << 4: Color.BEIGE,
+	2 << 5: Color.BISQUE,
+	2 << 6: Color.BLUE,
+	2 << 7: Color.BLUE_VIOLET,
+	2 << 8: Color.BROWN,
+	2 << 9: Color.REBECCA_PURPLE,
+	2 << 10: Color.VIOLET,
+	2 << 11: Color.CADET_BLUE,
+	2 << 12: Color.DARK_GOLDENROD,
+	2 << 13: Color.DEEP_PINK,
+	2 << 14: Color.WEB_GREEN,
+}
 
 func _ready() -> void:
-	self.position = grid_pos * 64
-	label.text = String.num(number) 
+	refresh_view(grid_pos, number)
+	appear()
 
-func refresh_view(new_pos, new_number) -> void:
-	grid_pos = new_pos
+func refresh_view(new_grid_pos, new_number) -> void:
+	grid_pos = new_grid_pos
 	number = new_number
 
+	label.text = String.num(number)
 	self.position = grid_pos * 64
-	label.text = String.num(number) 
+	nine_patch_rect.modulate = color_dict[number]
+	#print(self.position, grid_pos * 64)
+	#move(grid_pos * 64)
+
+func appear():
+	if tween != null:
+		tween.kill()
+	scale = Vector2.ZERO
+	tween = create_tween()
+	tween.tween_property(self, "scale", Vector2.ONE, 0.2)
+
+func disappear(pos):
+	if tween != null:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(self, "scale", Vector2.ZERO, 0.2)
+	tween.tween_callback(free)
+
+func move(pos):
+	tween = create_tween()
+	tween.tween_property(self, "position", pos, 0.2)
--- G2048/2048_game.gd
+++ G2048/2048_game.gd
@@ -2,14 +2,21 @@ extends Control
 
 @onready var panel: Panel = $Panel
 const CELL = preload("res://G2048/cell.tscn")
-const max_count:int = 5 * 5
-var map = {}
+const max_count: int = 5 * 5
+const next_create_count: int = 2
+var cell_dict = {}
+
+enum E_DIR
+{
+	up, down, right, left
+}
+var cur_dir = E_DIR.up
 
 func instantiate_cell(x, y):
 	var cell = CELL.instantiate()
 	cell.grid_pos = Vector2i(x, y)
 	panel.add_child(cell)
-	map[cell.get_instance_id()] = cell
+	cell_dict[cell.get_instance_id()] = cell
 
 func _ready() -> void:
 	random_create_cell(3)
@@ -17,53 +24,50 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_pressed():
 		merge_cell(event.keycode)
-		random_create_cell(2)
-		print(map)
-		
-func random_create_cell(count:int = 1):
+
+func is_empty(x: int, y: int) -> bool:
+	for k in cell_dict:
+		var cell = cell_dict[k]
+		if cell.grid_pos.x == x and cell.grid_pos.y == y:
+			return false
+	return true
+
+func random_create_cell(count: int = 1):
 	var array = []
-	for i in range(max_count):
-		if not map.erase(i):
-			array.append(i)
+	for number in range(max_count):
+		var x: int = number % 5
+		var y: int = number / 5
+		if is_empty(x, y):
+			array.append([x, y])
 	if array.size() >= count:
-		@warning_ignore("narrowing_conversion")
-		seed(Time.get_unix_time_from_system())
 		array.shuffle()
 		for i in range(count):
-			var number = array[i]
-			var x:int = number % 5
-			var y:int = number / 5
-			instantiate_cell(x, y)
+			var data = array[i]
+			instantiate_cell(data[0], data[1])
 	else:
-		pass 
+		print("你输了~")
+ 
 
-func merge_cell(keycode:int):
-	var merge_map = {}
-	var dir = null
-	var add = null
-	match(keycode):
+func merge_cell(keycode: int):
+	match (keycode):
 		KEY_UP:
-			dir = "x"
-			add = 1
+			cur_dir = E_DIR.up
 		KEY_DOWN:
-			dir = "x"
-			add = -1
+			cur_dir = E_DIR.down
 		KEY_RIGHT:
-			dir = "y"
-			add = -1
+			cur_dir = E_DIR.right
 		KEY_LEFT:
-			dir = "y"
-			add = 1
+			cur_dir = E_DIR.left
 		_:
 			return
+
+	var merge_cell_list = select_and_sort()
 	
-	merge_map = select_and_sort(dir, add, merge_map)
-	
-	for i in merge_map:
-		var array = merge_map[i]
+	for i in merge_cell_list:
+		var array = merge_cell_list[i]
 		var new_array = []
 
-		if add > 0 :
+		if cur_dir == E_DIR.right or cur_dir == E_DIR.down:
 			for ii in range(array.size(), 0, -1):
 				var cell = array[ii - 1]
 				new_array = add_new_array(new_array, cell)
@@ -71,7 +75,10 @@ func merge_cell(keycode:int):
 			for ii in range(array.size()):
 				var cell = array[ii]
 				add_new_array(new_array, cell)
-		update_cell_data(new_array, dir, add)
+			
+		update_cell_data(new_array)
+
+	random_create_cell(next_create_count)
 
 func add_new_array(new_array, cell):
 	if new_array.size() == 0:
@@ -80,39 +87,44 @@ func add_new_array(new_array, cell):
 		if new_array[-1].number == cell.number:
 			new_array[-1].number += cell.number
 			var idx = cell.get_instance_id()
-			map.erase(idx)
-			cell.free()
+			cell_dict.erase(idx)
+			cell.disappear(new_array[-1].position)
 		else:
 			new_array.append(cell)
 	return new_array
 
-func update_cell_data(new_array, dir, add):
+func update_cell_data(new_array):
 	for ii in range(new_array.size()):
 		var new_pos = Vector2i.ZERO
-		if dir == "x" :
+		if cur_dir == E_DIR.down:
 			new_pos.x = new_array[ii].grid_pos.x
-			if add > 0 :
-				new_pos.y = ii
-			else:
-				new_pos.y = 4 - ii
-		else:
+			new_pos.y = 4 - ii
+		elif cur_dir == E_DIR.up:
+			new_pos.x = new_array[ii].grid_pos.x
+			new_pos.y = ii
+		elif cur_dir == E_DIR.right:
+			new_pos.x = 4 - ii
 			new_pos.y = new_array[ii].grid_pos.y
-			if add > 0 :
-				new_pos.x = ii
-			else:
-				new_pos.x = 4 - ii
+		else:
+			new_pos.x = ii
+			new_pos.y = new_array[ii].grid_pos.y
 		new_array[ii].refresh_view(new_pos, new_array[ii].number)
 
-func select_and_sort(dir, add, merge_map):
-	for key in map.keys():
-		var cell = map[key]
-		if not merge_map.has(cell.grid_pos[dir]):
-			merge_map[cell.grid_pos[dir]] = []
-		var list = merge_map.get(cell.grid_pos[dir])
+func select_and_sort():
+	var condition = cur_dir == E_DIR.left or cur_dir == E_DIR.right
+	var k = "y" if condition else "x"
+	var merge_map = {}
+	for key in cell_dict.keys():
+		var cell = cell_dict[key]
+		if not merge_map.has(cell.grid_pos[k]):
+			merge_map[cell.grid_pos[k]] = []
+
+		var list = merge_map.get(cell.grid_pos[k])
+		
 		list.insert(list.bsearch_custom(cell, func(a, b):
-			if add > 0: 
-				return a.grid_pos[dir] > b.grid_pos[dir]
+			if cur_dir == E_DIR.up or cur_dir == E_DIR.left:
+				return a.grid_pos[k] > b.grid_pos[k]
 			else:
-				return a.grid_pos[dir] < b.grid_pos[dir]
+				return a.grid_pos[k] < b.grid_pos[k]
 			, true), cell)
 	return merge_map
